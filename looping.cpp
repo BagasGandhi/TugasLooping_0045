2c11988e1c62fc14e793341dbb52730f707ed9b1
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-04-01 14:47:00 +0800

    add fibonacci

--- looping.cpp
+++ looping.cpp
@@ -14,6 +14,17 @@ bool isPrima(int n) {
     return true;
 }
 
+bool isFibonacci(int n) {
+    int a = 0, b = 1, c = 0;
+    while (c <= n) {
+        if (c == n) return true;
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return false;
+}
+
 void tampilPrima() {
     if (isPrima(angka))
         cout << "Prima\n";
